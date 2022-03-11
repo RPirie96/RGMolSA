47c6d4199c9eabb75e853334908027b85f6845d7
RPirie96 <56546141+RPirie96@users.noreply.github.com>
2022-03-11 15:25:16 +0000

    add clean get_input

--- scripts/get_inputs.py
+++ scripts/get_inputs.py
@@ -1,31 +1,35 @@
-# script to get initial inputs
-import numpy as np
-from rdkit import Chem
-from rdkit.Chem import rdMolDescriptors
+"""
+script to get initial inputs
+
+Functions:
+- get_mol_info: script to convert molecule into matrix constructs
+
+Exceptions:
+- TypeError: raised if RDKit cannot generate 3D coordinates for molecule
+"""
+
 from collections import namedtuple
+import numpy as np
+
+from rdkit import Chem
+
+from conf_gen import embed_3d
 
 
 def get_mol_info(mol):
     """
-        Function which reads in ROMol structure, replaces rings with spheres and outputs no atoms, 3D centres,
-        adjacency matrix and list of radii
+    Function to convert molecule into matrices of centres, radii and adjacency with
+    rings replaced.
 
-        :param mol:
-        :return adjacency_matrix, no_atoms, centres, radii:
+    @param mol:
+    @return: matrix constructs describing molecule with rings replaced
     """
+    # check for 3D coords
+    if '3D' not in Chem.MolToMolBlock(mol).split("\n")[1]:
+        mol = embed_3d(mol)
 
-    # check for 3D coords, try embed if not
-    if '3D' not in Chem.MolToMolBlock(mol).split("\n")[1]:  # check if coords are 3D, get conf if not
-        ps = AllChem.ETKDGv3()
-        ps.useRandomCoords = True  # use random coordinates to help with issue of failed embeddings
-        ps.maxAttempts = 1000  # ignore smoothing failures, should be sufficient in most cases
-
-        mol = Chem.AddHs(mol)
-        AllChem.EmbedMolecule(mol, ps)
-        mol = Chem.RemoveHs(mol)
-
-    # if embed still fails, raise TypeError
-    if '3D' not in Chem.MolToMolBlock(mol).split("\n")[1]:  # check that the above embed has worked
+    # check that the above embed has worked
+    if '3D' not in Chem.MolToMolBlock(mol).split("\n")[1]:
         raise TypeError('RDKit cannot produce 3D coordinates for this molecule')
 
     # number of atoms in molecule
@@ -36,18 +40,21 @@ def get_mol_info(mol):
     centres = np.array([[p.x, p.y, p.z] for p in pos])
 
     # Atomic radii
-    radii = np.array([0.6*Chem.GetPeriodicTable().GetRvdw(a.GetAtomicNum()) for a in mol.GetAtoms()])
+    radii = np.array([0.6 * Chem.GetPeriodicTable().GetRvdw(a.GetAtomicNum()) for a in mol.GetAtoms()])
 
     # Adjacency matrix
     adjacency_matrix = Chem.rdmolops.GetAdjacencyMatrix(mol)
 
     # get the number of rings in the molecule
-    num_rings = rdMolDescriptors.CalcNumRings(mol)
+    num_rings = Chem.rdMolDescriptors.CalcNumRings(mol)
 
     # if the molecule contains rings
     if num_rings != 0:
-        indices = [a.GetIdx() for a in mol.GetAtoms()]  # list of all atom indices
-        rings = [list(mol.GetRingInfo().AtomRings()[i]) for i in range(num_rings)]  # list of ring indices
+        # list of all atom indices
+        indices = [a.GetIdx() for a in mol.GetAtoms()]
+
+        # list of ring indices
+        rings = [list(mol.GetRingInfo().AtomRings()[i]) for i in range(num_rings)]
         ring_pos = list(set(sorted(
             [index for sublist in rings for index in sublist])))  # this creates a sorted list of ring indexes
         non_ring = [x for x in indices if x not in ring_pos]
@@ -71,6 +78,7 @@ def get_mol_info(mol):
         radii = np.append(np.full(len(rings), 2.25), np.delete(radii, ring_pos))  # remove ring atom radii and add ring
 
         new_am = np.zeros((no_atoms, no_atoms), dtype=int)  # define empty matrix of new size
+
         # intersection between rings
         for i in range(len(rings)):
             for j in range(i + 1, len(rings)):
@@ -79,18 +87,21 @@ def get_mol_info(mol):
                         if adjacency_matrix[k][l] == 1 and new_am[i][j] == 0:
                             new_am[i][j] = 1
                             new_am[j][i] = 1
+
             # intersection between ring and non ring
             for k in rings[i]:
                 for l in range(len(non_ring)):
                     if adjacency_matrix[k][non_ring[l]] == 1 and new_am[i][updated_non_ring[l]] == 0:
                         new_am[i][updated_non_ring[l]] = 1
                         new_am[updated_non_ring[l]][i] = 1
+
         # intersection between non ring
         for i in range(len(non_ring)):
             for j in range(len(non_ring)):
                 if adjacency_matrix[non_ring[i]][non_ring[j]] == 1 and new_am[updated_non_ring[i]][updated_non_ring[j]] == 0:
                     new_am[updated_non_ring[i]][updated_non_ring[j]] = 1
                     new_am[updated_non_ring[j]][updated_non_ring[i]] = 1
+
     else:
         new_am = adjacency_matrix
     inputs = namedtuple('input', ['no_atoms', 'radii', 'centres', 'adjacency_matrix'])
