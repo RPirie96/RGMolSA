f8394925c0a38fdbe8c85489088cc0ee2002bb26
RPirie96 <56546141+RPirie96@users.noreply.github.com>
2022-03-11 13:51:57 +0000

    add clean get_descriptor script

--- scripts/get_descriptor.py
+++ scripts/get_descriptor.py
@@ -1,4 +1,9 @@
-# main script to return the descriptor for a single molecule
+"""
+main script to return the descriptor for a single molecule
+
+Functions:
+- get_descriptor: helper function to run all modules involved in calculating RGMolSA descriptor
+"""
 
 import numpy as np
 from numpy import linalg as la
@@ -8,44 +13,101 @@ from basesphere import get_base_sphere, get_levels, get_area, rescale_inputs, ba
 from stereo_projection import get_stereographic_projection
 from b_matrix import get_b_mat
 from a_matrix import get_a_mat
+from utils import cut_10
+
 
 def get_descriptor(mol):
-    """ Function to generate shape descriptor for a single molecule"""
 
-    # get centres, radii, adjacency matrix and no. atoms
-    inputs = get_mol_info(mol)
+    """
+    Function to generate shape descriptor for a single molecule
 
-    # get base sphere and re-centre on origin
-    base = get_base_sphere(inputs.centres)
+    @param mol:
+    @return: descriptor, or error type
+    """
 
-    # get levels within molecule
-    levels = get_levels(inputs.adjacency_matrix, inputs.no_atoms, base.base_sphere)
+    try:
+        # get centres, radii, adjacency matrix and no. atoms
+        inputs = get_mol_info(mol)
 
-    # get molecule area
-    mol_area = get_area(inputs.adjacency_matrix, base.centres, inputs.no_atoms, inputs.radii)
+        # get base sphere and re-centre on origin
+        base = get_base_sphere(inputs.centres)
 
-    # rescale inputs so molecule has surface area equivalent to a unit sphere
-    rescaled = rescale_inputs(mol_area.area, base.centres, inputs.radii, mol_area.lam)
+        # get levels within molecule
+        levels = get_levels(inputs.adjacency_matrix, inputs.no_atoms, base.base_sphere)
 
-    # error handling to account for cases where there is an atom over the north pole
-    error = base_error(levels, inputs, base, rescaled)
+        # get molecule area
+        mol_area = get_area(inputs.adjacency_matrix, base.centres, inputs.no_atoms, inputs.radii)
 
-    # perform 'piecewise stereographic projection' to move molecule into CP^n
-    stereo_proj = get_stereographic_projection(levels, inputs, rescaled, error.centres_r)
+        # rescale inputs so molecule has surface area equivalent to a unit sphere
+        rescaled = rescale_inputs(mol_area.area, base.centres, inputs.radii, mol_area.lam)
 
-    # get b matrix
-    b_mat = get_b_mat()
+        # error handling to account for cases where there is an atom over the north pole
+        error = base_error(levels, inputs, base, rescaled)
 
-    # get a matrix
-    a_mat = get_a_mat(inputs.no_atoms, stereo_proj, error)
+        # perform 'piecewise stereographic projection' to move molecule into CP^n
+        stereo_proj = get_stereographic_projection(levels, inputs, rescaled, error.centres_r)
 
-    # calculate final descriptor
-    c_mat = np.matmul(la.inv(a_mat), b_mat)
-    e_val, w = la.eig(c_mat)
-    e_val = sorted(e_val)
+        # get b matrix
+        b_mat = get_b_mat()
 
-    e_val[0] = 10000 / mol_area.area  # change 1st eigenvalue from 0 to area to account for re-scale
+        # get a matrix
+        a_mat = get_a_mat(inputs.no_atoms, stereo_proj, error)
 
-    e_val = [round(num, 3) for num in e_val]
+        # calculate final descriptor
+        c_mat = np.matmul(la.inv(a_mat), b_mat)
 
-    return e_val
+        try:
+            e_val, e_fun = la.eig(c_mat)
+
+        except np.linalg.LinAlgError:
+
+            # remove > level 10 spheres
+            new_inputs = cut_10(inputs, error)
+
+            # get base sphere and re-centre on origin
+            base = get_base_sphere(new_inputs.centres)
+
+            # get levels within molecule
+            levels = get_levels(new_inputs.adjacency_matrix, new_inputs.no_atoms, base.base_sphere)
+
+            # get molecule area
+            mol_area = get_area(new_inputs.adjacency_matrix, base.centres,
+                                new_inputs.no_atoms, new_inputs.radii)
+
+            # rescale inputs so molecule has surface area equivalent to a unit sphere
+            rescaled = rescale_inputs(mol_area.area, base.centres, new_inputs.radii, mol_area.lam)
+
+            # error handling to account for cases where there is an atom over the north pole
+            error = base_error(levels, new_inputs, base, rescaled)
+
+            # perform piecewise stereographic projection to move molecule into CP^n
+            stereo_proj = get_stereographic_projection(levels, new_inputs, rescaled, error.centres_r)
+
+            # get b matrix
+            b_mat = get_b_mat()
+
+            # get a matrix
+            a_mat = get_a_mat(new_inputs.no_atoms, stereo_proj, error)
+
+            # calculate final descriptor
+            c_mat = np.matmul(la.inv(a_mat), b_mat)
+
+            e_val, e_fun = la.eig(c_mat)
+
+        e_val = sorted(e_val)
+
+        # change 1st eigenvalue from 0 to area to account for re-scale
+        e_val[0] = 10000 / mol_area.area
+
+        if e_val[1] < 0:  # account for final numerical instabilities
+            e_val[1] = 0
+
+        e_val = [round(num, 3) for num in e_val]
+
+        return e_val
+
+    except TypeError:
+        return "TypeError"
+
+    except ValueError:
+        return "ValueError"
